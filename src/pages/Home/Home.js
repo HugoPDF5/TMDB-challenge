246f76d738c56381a59cd7667790d0161c34b036
Hugo Patrício <hugopatricio51@gmail.com>
2022-04-01 02:27:37 -0300

    Rendering the data dynamically and adding modal

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,29 +1,82 @@
 import './home.css'
+import { useEffect, useState } from "react";
+import { callAPI } from '../../services/movies_api'
+import { Button, Modal } from 'react-bootstrap'
 
-function Home(props){
 
-    return (
-      <div className="container">
-          <main> 
-            <div className="movies">
-            <table>
+function Home() {
+
+  const [movies, setMovies] = useState([])
+  const [show, setShow] = useState(false);
+  const [itemSelected, setItemSelected] = useState({})
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  useEffect(() => {
+    receiveMovies()
+  }, [])
+
+
+  const receiveMovies = async () => {
+    let moviesArray = await callAPI.getMoviesByRate()
+    setMovies(moviesArray)
+  }
+
+  function itemSelecionado(event) {
+    setItemSelected(event)
+    handleShow()
+  }
+
+  return (
+    <div className="container">
+      <main>
+        <div className="movies">
+          <table>
+            <thead>
               <tr>
-                <td> Name </td>
-                <td> Rating </td>
-                <td> Genre </td>
+                <th> Name </th>
+                <th> Rating </th>
               </tr>
-              
-          
-            </table>
-            </div>
-            <div className="search">
-            <h2>Search:</h2>
-            <input type="text"></input>
-            </div>
-          </main>
+            </thead>
 
-      </div>
-    )
+            <tbody>
+              {movies.map(item => {
+                return <tr>
+                  <td onClick={e => itemSelecionado(item)}>  {item.title} </td>
+                  <td> {item.vote_average} </td>
+                </tr>
+              })}
+            </tbody>
+
+          </table>
+        </div>
+        <div className="search">
+          <h2>Search:</h2>
+          <input type="text"></input>
+        </div>
+      </main>
+
+      <>
+
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+          <Modal.Title>{itemSelected.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Rating: {itemSelected.vote_average}</p>
+            <p>Release Date: {itemSelected.release_date}</p>
+            <p>Overview: {itemSelected.overview}</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Back
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    </div>
+  )
 }
 
 export default Home;
