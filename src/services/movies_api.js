5635d9ab76dc5d68e1bd9140e64964952d3c4b8f
Hugo Patrício <hugopatricio51@gmail.com>
2022-04-02 20:47:15 -0300

    Refactoring error handling of the request return

--- src/services/movies_api.js
+++ src/services/movies_api.js
@@ -1,6 +1,7 @@
 import api from './api';
 
 export const callAPI = {
+
     getMoviesByPopularity: async () => {
         const response = await api.get('movie/popular?api_key=fc45e0562ca750d5ca8a6f31cf72af4c')
             .then(res => {
@@ -28,13 +29,17 @@ export const callAPI = {
     getMovieByName: async (name_movie) => {
         const response = await api.get(`search/movie?api_key=fc45e0562ca750d5ca8a6f31cf72af4c&language=en-US&query=${name_movie}`)
             .then(res => {
+                if(res.data.total_results === 0) {
+                    return alert('Movie not found')
+                }
+
                 return res.data.results.filter((item, index) => {
                     if (index < 11) {
                         return item
                     }
                 })
             }).catch(e => {
-                console.error(e)
+                return alert('Please, enter the desired movie name.')
             })
             return response
     }
