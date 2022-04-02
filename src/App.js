04b198ae5cc3bf885d24ed6c2076c81e36140e4f
Hugo Patrício <hugopatricio51@gmail.com>
2022-04-02 20:48:09 -0300

    Editing routes to use only one menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import Home from './pages/Home/Home';
 import Genre from './pages/Genres/Genre'
 import Menu from './Components/Menu/Menu'
-import MenuGenres from './Components/Menu/MenuGenres'
 import {Router, Route} from 'react-router-dom'
 import history from './history';
 
 
 function App(){
 
-  
     return (
       
     <Router history={history}>
-     
-      <Route exact path="/">
+      
       <Menu />
+      <Route exact path="/">
         <Home />
       </Route>
 
       <Route path="/genres">
-        <MenuGenres />
         <Genre />
       </Route>
     </Router>
