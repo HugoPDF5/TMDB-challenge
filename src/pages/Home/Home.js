aee0296d6797d46dc329ca2fb2b33e8b685a07eb
Hugo Patrício <hugopatricio51@gmail.com>
2022-04-02 20:45:57 -0300

    Adding request return conditionals

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,7 @@
 import './home.css'
 import { useEffect, useState } from "react";
 import { callAPI } from '../../services/movies_api'
-import { Button, Modal } from 'react-bootstrap'
+import { Button, Form, FormControl, Modal } from 'react-bootstrap'
 import star from '../../assets/star.svg'
 import person from '../../assets/person.svg'
 import details from '../../assets/details.svg'
@@ -12,6 +12,7 @@ function Home() {
   const [movies, setMovies] = useState([])
   const [show, setShow] = useState(false);
   const [itemSelected, setItemSelected] = useState({})
+  const [wishMovie, setWishMovie] = useState('')
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -26,14 +27,35 @@ function Home() {
     setMovies(moviesArray)
   }
 
+  // Função que recebe os dados do filme antes de abrir o modal
   function getItemSelected(event) {
     setItemSelected(event)
     handleShow()
   }
 
+  // Requisição que recebe todos os filmes com contendo palavras que o usuário digitar
+  const relatedMovies = async () => {
+    const relatedMoviesArray = await callAPI.getMovieByName(wishMovie)
+    if (relatedMoviesArray === undefined) {
+      return
+    }
+    setMovies(relatedMoviesArray)
+  }
+
   return (
     <div className="container">
       <main>
+        <Form className="d-flex">
+          <FormControl
+            type="search"
+            placeholder="Search a movie"
+            className="me-2"
+            aria-label="Search"
+            onChange={e => setWishMovie(e.target.value)}
+          />
+          {console.log(wishMovie)}
+          <Button onClick={relatedMovies} variant="outline-success">Search</Button>
+        </Form>
         <div className="movies">
           <table>
             <thead>
@@ -57,23 +79,23 @@ function Home() {
         </div>
       </main>
 
-      <>
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
           <Modal.Title>{itemSelected.title}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <p>Rating: {itemSelected.vote_average} </p>
-            <p>Release Date: {itemSelected.release_date}</p>
-            <p>Overview: {itemSelected.overview}</p>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Back
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Rating: {itemSelected.vote_average} </p>
+          <p>Release Date: {itemSelected.release_date}</p>
+          <p>Overview: {itemSelected.overview}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Back
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
     </div>
   )
 }
