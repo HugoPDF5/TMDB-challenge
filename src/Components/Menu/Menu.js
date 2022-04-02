ad066cb2b987109c6a6fb6bf72174460744cacbc
Hugo Patrício <hugopatricio51@gmail.com>
2022-04-02 20:44:16 -0300

    Refactoring general menu

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,20 +1,9 @@
-import { useState } from 'react'
-import { Button, Container, Form, FormControl, Nav, Navbar } from 'react-bootstrap'
-import { callAPI } from '../../services/movies_api'
+import {  Container, Nav, Navbar } from 'react-bootstrap'
 
 
 function Menu() {
 
-    const [wishMovie, setWishMovie] = useState('')
-
-
-    const relatedMovies = async () => {
-        const response = await callAPI.getMovieByName(wishMovie)
-        console.log(response)
-    }
-
     return (
-
         <Navbar bg="light" expand="lg">
             <Container fluid>
                 <Navbar.Toggle aria-controls="navbarScroll" />
@@ -26,18 +15,7 @@ function Menu() {
                     >
                         <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/genres">Genre</Nav.Link>
-                    </Nav>
-                    <Form className="d-flex">
-                        <FormControl
-                            type="search"
-                            placeholder="Search a movie"
-                            className="me-2"
-                            aria-label="Search"
-                            onChange= {e => setWishMovie(e.target.value) }
-                            />
-                            {console.log(wishMovie)}
-                        <Button onClick={relatedMovies} variant="outline-success">Search</Button>
-                    </Form>
+                    </Nav> 
                 </Navbar.Collapse>
             </Container>
         </Navbar>
