3d26f73731d9b3010bfc5e34e2be7b6f52470387
Hugo Patrício <hugopatricio51@gmail.com>
2022-04-02 01:05:45 -0300

    Adding different menu in genres route

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-//import { getMoviesByRate, getMovieGenres, getMovieDetails, getMovieByName } from './services/movies_api'
 import Home from './pages/Home/Home';
 import Genre from './pages/Genres/Genre'
 import Menu from './Components/Menu/Menu'
+import MenuGenres from './Components/Menu/MenuGenres'
 import {Router, Route} from 'react-router-dom'
 import history from './history';
-
 
 
 function App(){
 
   
     return (
-    <Router  history={history}>
+      
+    <Router history={history}>
      
-      <Menu />
       <Route exact path="/">
-     
+      <Menu />
         <Home />
       </Route>
 
       <Route path="/genres">
+        <MenuGenres />
         <Genre />
       </Route>
-  
-
     </Router>
     )
 }
